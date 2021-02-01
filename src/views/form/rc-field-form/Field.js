9c4629c717789b616e29543039dfc4852691b02e
moshubai <939761443qq.com>
2021-02-02 00:15:46 +0800

    form

--- src/views/form/rc-field-form/Field.js
+++ src/views/form/rc-field-form/Field.js
@@ -1,31 +1,40 @@
-import React from 'react'
-import { Button } from 'antd'
+import React, { Component } from 'react'
+import FieldContext from './FieldContext'
 
-class Field extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
+// ? 因为等下我要用某个api
+export default class Field extends Component {
+  static contextType = FieldContext;
+
+  componentDidMount () {
+    this.unregisterFieldEntity = this.context.registerFieldEntity(this)
   }
 
-    static propTypes = {
-      // form: PropTypes.object
+  componentWillUnmount () {
+    if (this.unregisterFieldEntity) {
+      this.unregisterFieldEntity()
     }
+  }
 
-    componentDidMount () {
+  onStoreChange = () => {
+    this.forceUpdate()
+  };
 
+  getControlled = () => {
+    const { getFieldValue, setFieldsValue } = this.context
+    const { name } = this.props
+    return {
+      value: getFieldValue(name), // todo get 从数据仓库取值
+      onChange: e => {
+        const newValue = e.target.value
+        // todo set 修改数据仓库中的值
+        setFieldsValue({ [name]: newValue })
+      }
     }
+  };
 
-    render () {
-      console.log(this.props)
-      const { children } = this.props
-      return (
-            <React.Fragment>
-                <div>
-                    {children}
-                </div>
-            </React.Fragment>
-      )
-    }
+  render () {
+    const { children } = this.props
+    const returnChildNode = React.cloneElement(children, this.getControlled())
+    return returnChildNode
+  }
 }
-
-export default Field
--- src/views/form/rc-field-form/Form.js
+++ src/views/form/rc-field-form/Form.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import useForm from './useForm'
+import FieldContext from './FieldContext'
 
-const Form = ({ children }) => {
-  return children
+// 这里等下我想用hook
+export default function Form ({ children, onFinish, onFinishFailed, form }) {
+  const [formInstance] = useForm(form)
+
+  // React.useImperativeHandle(ref, () => formInstance)
+
+  formInstance.setCallbacks({ onFinish, onFinishFailed })
+
+  return (
+    <FieldContext.Provider value={formInstance}>
+      <form
+        onSubmit={e => {
+          // 提交
+          e.preventDefault()
+          formInstance.submit()
+        }}>
+        {children}
+      </form>
+    </FieldContext.Provider>
+  )
 }
-export default Form
